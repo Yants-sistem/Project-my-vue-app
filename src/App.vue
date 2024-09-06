3cf691f282ef2a296fc6c4ed032fd925eab7deb7
Alfian Rayhan <22rayhan@siswa.smktiannajiyah.sch.id>
2024-09-06 17:51:19 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold text-blue-600">
+      Hello, TailwindCSS with Vue 3!
+    </h1>
+  </div>
+</template>
